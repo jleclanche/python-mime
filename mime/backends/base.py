ccf17afce696e82fe8185f94134038b50bb7ccf5
Jerome Leclanche <jerome@leclan.ch>
2011-02-20 09:13:30 +0000

    Implement non-mime equality for BaseMime

--- mime/backends/base.py
+++ mime/backends/base.py
@@ -5,3 +5,8 @@ Base MimeType class
 class BaseMime(object):
 	DEFAULT_TEXT = "text/plain"
 	DEFAULT_BINARY = "application/octet-stream"
+	
+	def __eq__(self, other):
+		if isinstance(other, BaseMimeType):
+			return self.name() == other.name()
+		return self.name() == other
