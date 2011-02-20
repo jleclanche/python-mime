e08ddb0c3d9453692625470c5847b64803522076
Jerome Leclanche <jerome@leclan.ch>
2011-02-20 16:48:42 +0000

    Implement XDG_DATA_DIRS support instead of hardcoding /usr/share/mime and incrementally parse icons and globs files

--- mime/backends/linux.py
+++ mime/backends/linux.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Implementation of the XDG Shared MIME Info spec version 0.20.
-http://www.freedesktop.org/standards/shared-mime-info-spec/
+http://standards.freedesktop.org/shared-mime-info-spec/shared-mime-info-spec-0.20.html
 
 Loosely based on python-xdg and following the Qt code style.
 
@@ -11,43 +11,55 @@ update-mime-database command, which is provided by the freedesktop.org
 shared mime database package.
 """
 
+import os
 from fnmatch import fnmatch
 from xml.dom import minidom, XML_NAMESPACE
 from .base import BaseMime
 
 FREEDESKTOP_NS = "http://www.freedesktop.org/standards/shared-mime-info"
 
+HOME = os.path.expanduser("~")
+XDG_DATA_HOME   = os.environ.get("XDG_DATA_HOME", os.path.join(HOME, ".local", "share"))
+XDG_DATA_DIRS   = set([XDG_DATA_HOME] + os.environ.get("XDG_DATA_DIRS", "/usr/local/share:/usr/share").split(":"))
+# XDG_CONFIG_HOME = os.environ.get("XDG_CONFIG_HOME", os.path.join(HOME, ".config"))
+# XDG_CONFIG_DIRS = set([XDG_CONFIG_HOME] + os.environ.get("XDG_CONFIG_DIRS", "/etc/xdg").split(":"))
+# XDG_CACHE_HOME  = os.environ.get("XDG_CACHE_HOME", os.path.join(HOME, ".cache"))
+
+def getFiles(name):
+	ret = []
+	for dir in XDG_DATA_DIRS:
+		path = os.path.join(dir, "mime", name)
+		if os.path.exists(path):
+			ret.append(path)
+	return ret
+
 class GlobsFile(object):
 	"""
 	/usr/share/mime/globs2
 	"""
-	def __init__(self, path):
-		self.__file = open(path, "r")
-		
+	def __init__(self):
 		self.__matches = []
 		self.__literals = {}
-		
-		self.__parse()
-		self.__file.close()
 	
-	def __parse(self):
-		for line in self.__file:
-			if line.startswith("#"): # comment
-				continue
-			
-			if line.endswith("\n"):
-				line = line[:-1]
-			
-			weight, _, line = line.partition(":")
-			mime, _, line = line.partition(":")
-			glob, _, line = line.partition(":")
-			flags, _, line = line.partition(":")
-			flags = flags and flags.split(",") or []
-			
-			self.__matches.append((int(weight), mime, glob, flags))
-			
-			if "*" not in glob and "?" not in glob and "[" not in glob:
-				self.__literals[glob] = len(self.__matches)
+	def parse(self, path):
+		with open(path, "r") as file:
+			for line in file:
+				if line.startswith("#"): # comment
+					continue
+				
+				if line.endswith("\n"):
+					line = line[:-1]
+				
+				weight, _, line = line.partition(":")
+				mime, _, line = line.partition(":")
+				glob, _, line = line.partition(":")
+				flags, _, line = line.partition(":")
+				flags = flags and flags.split(",") or []
+				
+				self.__matches.append((int(weight), mime, glob, flags))
+				
+				if "*" not in glob and "?" not in glob and "[" not in glob:
+					self.__literals[glob] = len(self.__matches)
 	
 	def match(self, name):
 		if name in self.__literals:
@@ -67,39 +79,41 @@ class GlobsFile(object):
 		weight, mime, glob = max(matches, key=lambda (weight, mime, glob): (weight, len(glob)))
 		return mime
 
+GLOBS = GlobsFile()
+for f in getFiles("globs2"):
+	GLOBS.parse(f)
+
+
 class IconsFile(object):
 	"""
 	/usr/share/mime/icons
 	/usr/share/mime/generic-icons
 	"""
-	def __init__(self, path):
-		self.__file = open(path, "r")
-		
+	def __init__(self):
 		self.__icons = {}
-		
-		self.__parse()
-		self.__file.close()
 	
-	def __parse(self):
-		for line in self.__file:
-			if line.endswith("\n"):
-				line = line[:-1]
-			
-			mime, icon = line.split(":")
-			self.__icons[mime] = icon
+	def parse(self, path):
+		with open(path, "r") as file:
+			for line in file:
+				if line.endswith("\n"):
+					line = line[:-1]
+				
+				mime, icon = line.split(":")
+				self.__icons[mime] = icon
 	
 	def get(self, name):
 		return self.__icons.get(name)
 
+ICONS = IconsFile()
+for f in getFiles("generic-icons"):
+	ICONS.parse(f)
+
+
 class MimeType(BaseMime):
-	
-	BASE = "/usr/share/mime/"
-	GLOBS = GlobsFile(BASE + "globs2")
-	ICONS = IconsFile(BASE + "generic-icons")
 	
 	@classmethod
 	def fromName(cls, name):
-		mime = cls.GLOBS.match(name)
+		mime = GLOBS.match(name)
 		if mime:
 			return cls(mime)
 	
@@ -114,7 +128,7 @@ class MimeType(BaseMime):
 		return self._comment
 	
 	def genericIcon(self):
-		return self.ICONS.get(self.name())
+		return ICONS.get(self.name())
 	
 	def parent(self):
 		pass
