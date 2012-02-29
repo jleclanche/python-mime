c68f87fd80ff18eadbfd87d3c7a6432e636f56c1
Jerome Leclanche <jerome@leclan.ch>
2012-02-29 03:03:51 +0000

    Add tests for blockdevice, chardevice and directory inodes

--- tests/__init__.py
+++ tests/__init__.py
@@ -25,6 +25,12 @@ u'document texte brut'
 'text/x-c++src'
 >>> MimeType.fromName("foo.c").name()
 'text/x-csrc'
+>>> MimeType.fromContent("/dev/ram0").name()
+'inode/blockdevice'
+>>> MimeType.fromContent("/dev/null").name()
+'inode/chardevice'
+>>> MimeType.fromContent("/").name()
+'inode/directory'
 >>> MimeType("text/x-lua").comment()
 u'Lua script'
 >>> MimeType("application/x-does-not-exist")
